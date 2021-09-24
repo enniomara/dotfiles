e6401eb9e161cb161e4966f51c06c9118ac36604
Ennio Mara <enniomara@users.noreply.github.com>
2021-09-24 15:27:40 +0200

    Add initial nvim config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,18 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd 'packadd packer.nvim'
+end
+
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function()
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+	use 'scrooloose/nerdtree'
+	use {
+		'kyazdani42/nvim-tree.lua',
+		requires = 'kyazdani42/nvim-web-devicons'
+	}
+end)
