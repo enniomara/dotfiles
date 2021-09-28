4bc92cd5e50d915e90f31b4fa58054b435b79f08
Ennio Mara <enniomara@users.noreply.github.com>
2021-09-28 22:38:40 +0200

    Add telescope mappings for file browser and live grep

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -5,3 +5,5 @@ end
 
 mapper("n", "<Leader>d", ":NvimTreeToggle<CR>")
 mapper("n", "<Leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>")
+mapper("n", "<Leader>fd", "<cmd>lua require('telescope.builtin').file_browser()<CR>")
+mapper("n", "<Leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<CR>")
