e7e6d7be6e02186c12356fb73d99d4d80991e77b
Ennio Mara <enniomara@users.noreply.github.com>
2021-10-02 21:05:44 +0200

    Setup bufferline plugin

---
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,13 @@
+require("bufferline").setup{
+    options = {
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = "",
+                padding = 1
+            }
+
+        },
+        tab_size = 20
+    }
+}
