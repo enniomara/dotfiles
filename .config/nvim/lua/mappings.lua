dbf0bc84b45cc5a486f1c0eff99a63976e510933
Ennio Mara <enniomara@users.noreply.github.com>
2021-10-02 21:17:05 +0200

    Add tmux zoom like mapping

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -23,3 +23,6 @@ mapper("n", "<Leader>5", ":BufferLineGoToBuffer 6<CR>")
 mapper("n", "<Leader>5", ":BufferLineGoToBuffer 7<CR>")
 mapper("n", "<Leader>5", ":BufferLineGoToBuffer 8<CR>")
 mapper("n", "<Leader>5", ":BufferLineGoToBuffer 5<CR>")
+
+-- Tmux-zoom like feature, full-screens the current pane
+mapper("n", "<C-W>z", ":tab split <CR>")
