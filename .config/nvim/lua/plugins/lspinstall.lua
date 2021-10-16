3b77fdd4ca07d26ae399bcb54a6d5e71992dd6a6
Ennio Mara <enniomara@users.noreply.github.com>
2021-10-16 18:36:54 +0200

    Correct lua install configuration

--- .config/nvim/lua/plugins/lspinstall.lua
+++ .config/nvim/lua/plugins/lspinstall.lua
@@ -4,18 +4,20 @@ lsp_installer.on_server_ready(function(server)
 	local opts = {}
 
 	if server.name == "sumneko_lua" then
-		opts.Lua = {
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { "vim" },
-			},
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-			},
-			-- Do not send telemetry data containing a randomized but unique identifier
-			telemetry = {
-				enable = false,
+		opts.settings = {
+			Lua = {
+				diagnostics = {
+					-- Get the language server to recognize the `vim` global
+					globals = { "vim" },
+				},
+				workspace = {
+					-- Make the server aware of Neovim runtime files
+					library = vim.api.nvim_get_runtime_file("", true),
+				},
+				-- Do not send telemetry data containing a randomized but unique identifier
+				telemetry = {
+					enable = false,
+				},
 			},
 		}
 	end
