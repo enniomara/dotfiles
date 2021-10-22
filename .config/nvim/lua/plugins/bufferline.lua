64c7aefb1bb6b970299b8f6e62715d77d2fd4d34
Ennio Mara <enniomara@users.noreply.github.com>
2021-10-22 19:55:04 +0200

    Turn on diagnostica for bufferline

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -8,5 +8,10 @@ require("bufferline").setup({
 			},
 		},
 		tab_size = 20,
+		diagnostics = "nvim_lsp",
+		diagnostics_indicator = function(count, level, _, _)
+			local icon = level:match("error") and " " or " "
+			return " " .. icon .. count
+		end,
 	},
 })
