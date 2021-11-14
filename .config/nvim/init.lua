688914a00faa613ad26b2c4abf18404030f89cc4
Ennio Mara <enniomara@users.noreply.github.com>
2021-11-14 18:54:13 +0100

    Pin all nvim plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -34,18 +34,16 @@ require("packer").startup(function(use)
 		config = function()
 			require("plugins.telescope")
 		end,
-		lock = true,
 	})
 	use({
 		"nvim-telescope/telescope-fzf-native.nvim",
 		branch = "main",
 		commit = "59e38e1661ffdd586cb7fc22ca0b5a05c7caf988",
 		run = "make",
-		lock = true,
 	})
 	use({
 		"sudormrfbin/cheatsheet.nvim",
-		branch = "main",
+		branch = "master",
 		commit = "3f0b718a030dd2c2757e053345de4f130f213514",
 
 		requires = {
@@ -56,7 +54,6 @@ require("packer").startup(function(use)
 		config = function()
 			require("plugins.cheatsheet")
 		end,
-		lock = true,
 	})
 	use({
 		"akinsho/bufferline.nvim",
@@ -66,57 +63,73 @@ require("packer").startup(function(use)
 		config = function()
 			require("plugins.bufferline")
 		end,
-		lock = true,
 	})
 	use({
 		"ntpeters/vim-better-whitespace",
+		branch = "master",
+		commit = "c5afbe91d29c5e3be81d5125ddcdc276fd1f1322",
 		config = function()
 			require("plugins.vim-better-whitespace")
 		end,
-		lock = true,
 	})
 	use({
 		"lewis6991/gitsigns.nvim",
+		tag = "v0.3",
 		requires = {
 			"nvim-lua/plenary.nvim",
 		},
 		config = function()
 			require("plugins.gitsigns")
 		end,
-		lock = true,
 	})
 	use({
 		"nvim-treesitter/nvim-treesitter",
+		branch = "0.5-compat",
+		commit = "47cfda2c6711077625c90902d7722238a8294982",
 		run = ":TSUpdate",
 		event = "BufRead",
 		config = function()
 			require("plugins.treesitter")
 		end,
-		lock = true,
 	})
-	use({ "tpope/vim-fugitive", lock = true })
-	use({ "tpope/vim-unimpaired", lock = true })
-	use({ "tpope/vim-sleuth", lock = true })
+	use({
+		"tpope/vim-fugitive",
+		tag = "v3.4",
+	})
+	use({
+		"tpope/vim-unimpaired",
+		branch = "master",
+		commit = "e4006d68cd4f390efef935bc09be0ce3bd022e72",
+	})
+	use({
+		"tpope/vim-sleuth",
+		tag = "v1.2",
+	})
 
 	use({ -- used for formatting mainly
 		"jose-elias-alvarez/null-ls.nvim",
+		branch = "main",
+		commit = "a3ded9b1525b06136e58a95f4d9ed2f090c0f908",
 		requires = {
 			"nvim-lua/plenary.nvim",
 			"neovim/nvim-lspconfig",
 			"lewis6991/gitsigns.nvim",
 		},
-		lock = true,
 	})
 	use({
 		"neovim/nvim-lspconfig",
-		lock = true,
+		branch = "master",
+		commit = "e3d0992737985c52b93b0cef338895c164dd1cfb",
 	})
 	use({
 		"williamboman/nvim-lsp-installer",
-		lock = true,
+		branch = "main",
+		commit = "35d4b08d60c17b79f8e16e9e66f0d7693c99d612",
 	})
 	use({
 		"hrsh7th/nvim-cmp",
+		branch = "main",
+		commit = "4ecf2a24265626a2c00427394deb4747c7b9d5dc",
 		requires = {
 			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
@@ -127,35 +140,42 @@ require("packer").startup(function(use)
 		config = function()
 			require("plugins.cmp")
 		end,
-		lock = true,
 	})
-	use("rafamadriz/friendly-snippets")
+	use({
+		"rafamadriz/friendly-snippets",
+		branch = "main",
+		commit = "05bfa7681b8f11b664fab657001c2efb6f3ec85e",
+	})
 	use({
 		"folke/which-key.nvim",
+		branch = "main",
+		commit = "d3032b6d3e0adb667975170f626cb693bfc66baa",
 		config = function()
 			require("which-key").setup({})
 		end,
-		lock = true,
 	})
 	use({
 		"windwp/nvim-autopairs",
+		branch = "master",
+		commit = "b5d077d2a055485a00846a107c777321feed1d29",
 		config = function()
 			require("nvim-autopairs").setup({
 				disable_filetype = { "TelescopePrompt", "vim" },
 			})
 		end,
-		lock = true,
 	})
 	use({
 		"terrortylor/nvim-comment",
+		branch = "main",
+		commit = "6363118acf86824ed11c2238292b72dc5ef8bdde",
 		event = "BufRead",
 		config = function()
 			require("nvim_comment").setup()
 		end,
-		lock = true,
 	})
 	use({
 		"lukas-reineke/indent-blankline.nvim",
+		tag = "v2.10.5",
 		event = "BufRead",
 		config = function()
 			require("indent_blankline").setup({
@@ -165,17 +185,19 @@ require("packer").startup(function(use)
 				show_trailing_blankline_indent = false,
 			})
 		end,
-		lock = true,
 	})
 	use({
 		"rmagatti/auto-session",
+		branch = "main",
+		commit = "5f2a9d7e45133be33a1dae3580089e257824a907",
 		config = function()
 			require("auto-session").setup({})
 		end,
-		lock = true,
 	})
 	use({
 		"hoob3rt/lualine.nvim",
+		branch = "master",
+		commit = "e6b6caa93b900c86d8c299d92282feae3934ec39",
 		requires = { "kyazdani42/nvim-web-devicons", opt = true },
 		config = function()
 			require("lualine").setup({
@@ -198,13 +220,16 @@ require("packer").startup(function(use)
 				},
 			})
 		end,
-		lock = true,
 	})
 	use({
 		"ray-x/lsp_signature.nvim",
-		lock = true,
+		tag = "v0.1.1",
 	})
-	use({ "ggandor/lightspeed.nvim", lock = true })
+	use({
+		"ggandor/lightspeed.nvim",
+		branch = "main",
+		commit = "c0ebc350e65b1f7370ec2d5b6f83bdc1ce602761",
+	})
 end)
 
 require("options")
