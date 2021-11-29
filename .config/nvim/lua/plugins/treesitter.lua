117f67313e6d8ae7f1f330b9cb4391797671bdfc
Ennio Mara <enniomara@users.noreply.github.com>
2021-11-29 21:24:11 +0100

    Re enable yaml highlighting via treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,9 +2,6 @@ require("nvim-treesitter.configs").setup({
 	ensure_installed = "maintained",
 	highlight = {
 		enable = true,
-		disable = {
-			"yaml"
-		}
 	},
 	incremental_selection = {
 		enable = true,
