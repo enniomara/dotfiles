7f8680e29a43e8a4f70f51b5cb35727a838659d3
Ennio Mara <marae@axis.com>
2022-01-12 14:05:59 +0100

    Remove unused function
    Forgot to remove

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -1,9 +1,3 @@
-local function restore_nvim_tree()
-	local nvim_tree = require("nvim-tree")
-	nvim_tree.change_dir(vim.fn.getcwd())
-	nvim_tree.refresh()
-end
-
 require("auto-session").setup({
 	pre_save_cmds = { "NvimTreeClose" },
 })
