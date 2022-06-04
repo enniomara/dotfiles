ce0f590fb597d8a4f3bb4213f96c6d707f09df1c
Ennio Mara <enniomara@users.noreply.github.com>
2022-06-04 20:59:16 +0200

    Migrate to use lspconfig for server definition
    The old method where lspinstall configured them is now deprecated. See
    https://github.com/williamboman/nvim-lsp-installer/discussions/636

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -1,10 +1,79 @@
 local config = require("plugins.lsp.config")
+local lspconfig = require("lspconfig")
 
 -- nvim-cmp supports additional completion capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
-local lspinstall = require("plugins.lspinstall")
-lspinstall.setup(config.on_attach, capabilities)
+lspconfig.sumneko_lua.setup({
+	on_attach = config.on_attach,
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+})
+
+lspconfig.gopls.setup({
+	on_attach = config.on_attach,
+	capabilities = capabilities,
+	settings = {
+		gopls = {
+			buildFlags = {
+				"-tags=requireDB,requireAWS,aws",
+			},
+		},
+	},
+})
+
+lspconfig.yamlls.setup({
+	on_attach = config.on_attach,
+	capabilities = capabilities,
+	settings = {
+		yaml = {
+			customTags = {
+				"!And",
+				"!And sequence",
+				"!If",
+				"!If sequence",
+				"!Not",
+				"!Not sequence",
+				"!Equals",
+				"!Equals sequence",
+				"!Or",
+				"!Or sequence",
+				"!FindInMap",
+				"!FindInMap sequence",
+				"!Base64",
+				"!Join",
+				"!Join sequence",
+				"!Cidr",
+				"!Ref",
+				"!Sub",
+				"!Sub sequence",
+				"!GetAtt",
+				"!GetAZs",
+				"!ImportValue",
+				"!ImportValue sequence",
+				"!Select",
+				"!Select sequence",
+				"!Split",
+				"!Split sequence",
+			},
+		},
+	},
+})
 
 require("lsp_signature").setup({})
