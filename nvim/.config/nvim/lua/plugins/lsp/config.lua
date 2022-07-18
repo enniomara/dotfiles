8dffb7c8e4d41cdd0d6970d32e1ec979b1896d19
Ennio Mara <enniomara@users.noreply.github.com>
2022-07-18 10:20:31 +0200

    Use correct setloclist function

--- nvim/.config/nvim/lua/plugins/lsp/config.lua
+++ nvim/.config/nvim/lua/plugins/lsp/config.lua
@@ -25,7 +25,7 @@ M.on_attach = function(_, bufnr)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>e", "<cmd>lua vim.diagnostic.open_float({scope=\"line\"})<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.set_loclist()<CR>", opts)
+	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
 	vim.api.nvim_buf_set_keymap(
 		bufnr,
 		"n",
