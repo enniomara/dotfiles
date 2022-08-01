d6906d55de744c8c7661f56dddc11c96e0b502e9
Ennio Mara <enniomara@users.noreply.github.com>
2022-08-01 10:47:59 +0200

    Setup clojure lsp setup

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -84,5 +84,9 @@ lspconfig.tsserver.setup({
 	on_attach = config.on_attach,
 	capabilities = capabilities,
 })
+lspconfig.clojure_lsp.setup({
+	on_attach = config.on_attach,
+	capabilities = capabilities,
+})
 
 require("lsp_signature").setup({})
