e249cef7edbda4ba06f01146666683aeb73c5240
Ennio Mara <enniomara@users.noreply.github.com>
2022-10-25 23:49:13 +0200

    Use devshell for defining packages used by repo
    Much easier to use than nix and it does not pollute the environment with
    environment variables that change the default compiler.
    
    Had this problem with treesitter in nvim, where compiling failed since
    clang is the default compiler, which treesitter for some reason does not
    like.

--- flake.nix
+++ flake.nix
@@ -2,22 +2,24 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    devshell.url = "github:numtide/devshell";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      rec {
-        devShells.default =
-          pkgs.mkShell {
-            buildInputs = [
-              pkgs.stow
-            ];
-            # set the name that will be shown in the zsh prompt
-            name = "dotfiles";
-          };
-      }
-    );
+  outputs = { self, nixpkgs, flake-utils, devshell }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShell =
+        let pkgs = import nixpkgs {
+          inherit system;
+
+          overlays = [ devshell.overlay ];
+        };
+        in
+        pkgs.devshell.mkShell {
+          # set the name that will be shown in the zsh prompt
+          name = "dotfiles";
+          packages = [
+            pkgs.stow
+          ];
+        };
+    });
 }
