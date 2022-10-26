77f0b8429087020c326584990b3ffc0c8839135d
Ennio Mara <enniomara@users.noreply.github.com>
2022-10-26 18:05:54 +0200

    Use default_capabilities in nvim-cmp
    As it will be deprecated in the future.

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -5,8 +5,7 @@ local config = require("plugins.lsp.config")
 local lspconfig = require("lspconfig")
 
 -- nvim-cmp supports additional completion capabilities
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 lspconfig.sumneko_lua.setup({
 	on_attach = config.on_attach,
