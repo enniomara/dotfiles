0ce6046d1f41205f9b0678d4027fd4c3b1190cd5
Ennio Mara <enniomara@users.noreply.github.com>
2022-12-30 23:21:08 +0100

    Map <Leader>f; to keymaps instead of commands
    Never ued commands, keymaps seems to contain some more useful commands.

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -8,7 +8,7 @@ mapper("n", "<Leader>qq", ":Bdelete<CR>") -- close current buffer
 mapper("n", "<Leader>ff", ":Telescope find_files<CR>")
 mapper("n", "<Leader>fg", ":Telescope live_grep<CR>")
 mapper("n", "<Leader>fb", ":Telescope buffers<CR>")
-mapper("n", "<Leader>f;", ":Telescope commands<CR>")
+mapper("n", "<Leader>f;", ":Telescope keymaps<CR>")
 mapper("n", "<Leader>fs", ":Telescope sessions<CR>")
 mapper("n", "<Leader>fp", ":Telescope<CR>")
 
