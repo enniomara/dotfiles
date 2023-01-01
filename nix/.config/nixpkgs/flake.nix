f1cf78c69f08b064d544db94a471485955584f75
Ennio Mara <enniomara@users.noreply.github.com>
2023-01-01 12:34:05 +0100

    Add configuration for private laptop

--- nix/.config/nixpkgs/flake.nix
+++ nix/.config/nixpkgs/flake.nix
@@ -34,6 +34,19 @@
           # Optionally use extraSpecialArgs
           # to pass through arguments to home.nix
         };
+
+        legacyPackages.homeConfigurations.enniomara = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+
+          # Specify your home configuration modules here, for example,
+          # the path to your home.nix.
+          modules = [
+            ./home.nix
+          ];
+
+          # Optionally use extraSpecialArgs
+          # to pass through arguments to home.nix
+        };
       }
     );
 }
