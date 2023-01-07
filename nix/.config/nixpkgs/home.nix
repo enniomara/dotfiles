6956da3d0e9c794c1871693b557a56f5a13ac839
Ennio Mara <enniomara@users.noreply.github.com>
2023-01-07 20:45:20 +0100

    Configure tmux using nix

--- nix/.config/nixpkgs/home.nix
+++ nix/.config/nixpkgs/home.nix
@@ -23,6 +23,28 @@
     nix-direnv.enable = true;
   };
 
+  xdg.configFile."tmux/tmux.conf".text = pkgs.lib.strings.concatStrings (
+    pkgs.lib.strings.intersperse "\n" (
+      with pkgs.tmuxPlugins; [
+        (builtins.readFile (pkgs.substituteAll {
+          src = ../../../tmux/.tmux.conf;
+          sessionizerPath = ../../../tmux/bin/tmux-sessionizer;
+        }))
+        # load my custom theme
+        (builtins.readFile ../../../tmux/.tmux-theme.conf)
+
+        # install plugins - last
+        ''
+          run-shell ${sensible.rtp}
+          run-shell ${prefix-highlight.rtp}
+          run-shell ${better-mouse-mode.rtp}
+          run-shell ${yank.rtp}
+          run-shell ${pain-control.rtp}
+        ''
+      ]
+    )
+  );
+
   programs.neovim = {
     enable = true;
     plugins = [
