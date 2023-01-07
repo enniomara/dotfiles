57365bbfb654ea9e8cc741b018e05cf8dcde1e7c
Ennio Mara <enniomara@users.noreply.github.com>
2023-01-07 20:45:40 +0100

    Stop symlining tmux using stow

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-STOW_FOLDERS="hammerspoon,karabiner,kitty,nvim,tmux,nix"
+STOW_FOLDERS="hammerspoon,karabiner,kitty,nvim,nix"
 TARGET="$HOME"
 
 for folder in ${STOW_FOLDERS//,/ }
