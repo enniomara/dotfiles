c5176e2d6164fc611ca7becbb2ca904681c8a315
Ennio Mara <enniomara@users.noreply.github.com>
2023-01-09 14:48:31 +0100

    Add git config for work

--- nix/.config/nixpkgs/axis.nix
+++ nix/.config/nixpkgs/axis.nix
@@ -3,4 +3,16 @@
   programs.zsh.sessionVariables = {
     GOPRIVATE = "gittools.se.axis.com";
   };
+
+  programs.git = {
+    userName = "Ennio Mara";
+    userEmail = "marae@axis.com";
+    extraConfig = {
+      url = {
+        "ssh://marae@gittools.se.axis.com:29418/" = {
+          insteadOf = "https://gittools.se.axis.com/gerrit/a/";
+        };
+      };
+    };
+  };
 }
