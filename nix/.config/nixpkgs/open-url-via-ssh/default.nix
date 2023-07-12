b4fc3ebaa017c74ad17c2d9000e89bc71f767e48
Ennio Mara <enniomara@users.noreply.github.com>
2023-07-12 11:18:40 +0200

    Change alias for exporting browser variable
    Easier to remember if it starts with ssh

--- nix/.config/nixpkgs/open-url-via-ssh/default.nix
+++ nix/.config/nixpkgs/open-url-via-ssh/default.nix
@@ -3,7 +3,7 @@
   programs.zsh.shellAliases = {
     # so that the target environment knows that URLs should be sent to the ssh
     # server
-    exportSSHBrowser = "export BROWSER=\"${./client.sh} localhost 54821\"";
+    sshExportBrowser = "export BROWSER=\"${./client.sh} localhost 54821\"";
 
     # exports an alias that wraps around SSH. It starts a server which accepts
     # connections from the ssh client. Used to open URLs in the SSH client
