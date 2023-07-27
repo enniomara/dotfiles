e3e68da309197a69dc3effc84741c855802d206c
Ennio Mara <enniomara@users.noreply.github.com>
2023-07-27 13:32:44 +0200

    Remove ssh export browser alias
    It's done automatically and handled via a Nix module option

--- nix/.config/nixpkgs/open-url-via-ssh/default.nix
+++ nix/.config/nixpkgs/open-url-via-ssh/default.nix
@@ -14,10 +14,6 @@ let
 
   config = lib.mkIf cfg.enable {
     programs.zsh.shellAliases = {
-      # so that the target environment knows that URLs should be sent to the ssh
-      # server
-      sshExportBrowser = "export BROWSER=\"${./client.sh} localhost 54821\"";
-
       # exports an alias that wraps around SSH. It starts a server which accepts
       # connections from the ssh client. Used to open URLs in the SSH client
       # instead of the SSH server's browser.
