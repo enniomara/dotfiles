f6a10bd705f53c88db087e6947f79dc3a59c6369
Ennio Mara <enniomara@users.noreply.github.com>
2023-09-24 16:08:43 +0200

    Add activation script when changing macos settings

--- nix/.config/nixpkgs/configuration.nix
+++ nix/.config/nixpkgs/configuration.nix
@@ -45,6 +45,10 @@
     ];
   };
 
+  system.activationScripts.postUserActivation.text = ''
+    # Following line should allow us to avoid a logout/login cycle
+    /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+  '';
   system = {
     defaults = {
       NSGlobalDomain = {
