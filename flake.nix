d13589fdf95a2e6fa728019b495fe723d02ca6d9
Ennio Mara <enniomara@users.noreply.github.com>
2023-09-30 20:52:02 +0200

    Move devshell to separate module

--- flake.nix
+++ flake.nix
@@ -5,32 +5,12 @@
     devshell.url = "github:numtide/devshell";
   };
 
-  outputs = { self, nixpkgs, flake-utils, devshell }:
-    flake-utils.lib.eachDefaultSystem (system: {
-      devShell =
-        let pkgs = import nixpkgs {
-          inherit system;
+  outputs = inputs @ { self, nixpkgs, flake-utils, devshell }:
+    let
+      # the devshells used by this repo
+      devshells = (import ./modules/devShell.nix) inputs;
+    in
+    {
 
-          overlays = [ devshell.overlay ];
-        };
-        in
-        pkgs.devshell.mkShell {
-          # set the name that will be shown in the zsh prompt
-          name = "dotfiles";
-          packages = [
-            pkgs.stow
-          ];
-          commands = [
-            {
-              name = "rebuild-home";
-              command = "home-manager switch --flake \"$PRJ_ROOT/nix/.config/nixpkgs/\"";
-
-            }
-            {
-              name = "rebuild-darwin";
-              command = "darwin-rebuild switch --flake \"$PRJ_ROOT/nix/.config/nixpkgs/\"";
-            }
-          ];
-        };
-    });
+    } // devshells;
 }
---
+++ modules/devShell.nix
@@ -0,0 +1,29 @@
+{ self, nixpkgs, flake-utils, devshell }:
+flake-utils.lib.eachDefaultSystem (system: {
+  devShell =
+    let pkgs = import nixpkgs {
+      inherit system;
+
+      overlays = [ devshell.overlay ];
+    };
+    in
+    pkgs.devshell.mkShell {
+      # set the name that will be shown in the zsh prompt
+      name = "dotfiles";
+      packages = [
+        pkgs.stow
+      ];
+      commands = [
+        {
+          name = "rebuild-home";
+          command = "home-manager switch --flake \"$PRJ_ROOT/nix/.config/nixpkgs/\"";
+
+        }
+        {
+          name = "rebuild-darwin";
+          command = "darwin-rebuild switch --flake \"$PRJ_ROOT/nix/.config/nixpkgs/\"";
+        }
+      ];
+    };
+})
+
