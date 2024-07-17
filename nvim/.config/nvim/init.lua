27233606ba806316f63e4a1d0350425eff44bca5
Ennio Mara <enniomara@users.noreply.github.com>
2024-07-17 14:58:05 +0200

    Trial run of harpoon

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -625,6 +625,26 @@ require("lazy").setup({
 				openai_api_key = { "cat", os.getenv("HOME") .. "/.config/openai/api.key" },
 			},
 		},
+		{
+			"ThePrimeagen/harpoon",
+			branch = "harpoon2",
+			dependencies = { "nvim-lua/plenary.nvim" },
+			config = function()
+				local harpoon = require("harpoon")
+
+				harpoon:setup()
+				vim.keymap.set("n", "<leader>ke", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+				vim.keymap.set("n", "<leader>ka", function() harpoon:list():add() end)
+				vim.keymap.set("n", "<M-j>", function() harpoon:list():select(1) end)
+				vim.keymap.set("n", "<M-k>", function() harpoon:list():select(2) end)
+				vim.keymap.set("n", "<M-l>", function() harpoon:list():select(3) end)
+				vim.keymap.set("n", "<M-;>", function() harpoon:list():select(4) end)
+
+				vim.keymap.set("n", "<M-m>", function() harpoon:list():prev() end)
+				vim.keymap.set("n", "<M-,>", function() harpoon:list():next() end)
+
+			end,
+		},
 	},
 })
 
