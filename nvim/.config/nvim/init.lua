6232cc76b1c52b701cacf0b63f2c408ae81ab86c
Ennio Mara <enniomara@users.noreply.github.com>
2024-08-16 16:07:35 +0200

    Move linting to linting.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -48,17 +48,6 @@ require("lazy").setup({
 			version = "*",
 		},
 		{
-			"jose-elias-alvarez/null-ls.nvim",
-			dependencies = {
-				"nvim-lua/plenary.nvim",
-				"lewis6991/gitsigns.nvim",
-			},
-			event = { "BufRead", "BufNewFile" },
-			config = function()
-				require("config.lsp.null-ls")
-			end,
-		},
-		{
 			"neovim/nvim-lspconfig",
 			dependencies = {
 				{
