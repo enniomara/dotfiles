23f7b7870b63bdcf75e6c75b715f5553f7170f99
Ennio Mara <enniomara@users.noreply.github.com>
2024-08-16 16:05:59 +0200

    Move editing packages to editing.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -16,12 +16,6 @@ require("lazy").setup({
 	spec = {
 		{ import = "plugins" },
 		{
-			"ntpeters/vim-better-whitespace",
-			config = function()
-				require("config.vim-better-whitespace")
-			end,
-		},
-		{
 			"nvim-treesitter/nvim-treesitter",
 			dependencies = {
 				"nvim-treesitter/nvim-treesitter-textobjects",
@@ -82,53 +76,6 @@ require("lazy").setup({
 			event = { "BufReadPre", "BufNewFile" },
 		},
 		{
-			"hrsh7th/nvim-cmp",
-			dependencies = {
-				"L3MON4D3/LuaSnip",
-				"saadparwaiz1/cmp_luasnip",
-				"hrsh7th/cmp-nvim-lsp",
-				"hrsh7th/cmp-buffer",
-				"hrsh7th/cmp-cmdline",
-				"hrsh7th/cmp-path",
-				"windwp/nvim-autopairs",
-				"onsails/lspkind-nvim",
-			},
-			event = { "InsertEnter", "CmdlineEnter" },
-			config = function()
-				require("config.cmp")
-			end,
-		},
-		"rafamadriz/friendly-snippets",
-		{
-			"folke/which-key.nvim",
-			version = "*",
-			event = "VeryLazy",
-			config = function()
-				local wk = require("which-key")
-				wk.setup({})
-
-				local opts = { nowait = false, remap = false }
-				wk.add({
-					{ "<leader>g", group = "Git",     unpack(opts) },
-					{ "<leader>t", group = "Test",    unpack(opts) },
-					{ "<leader>d", group = "Debug",   unpack(opts) },
-					{ "<leader>f", group = "Find",    unpack(opts) },
-					{ "<leader>k", group = "Harpoon", unpack(opts) },
-					{ "<leader>l", group = "LSP",     unpack(opts) },
-					{ "<leader>a", group = "AI",      unpack(opts) },
-				})
-			end,
-		},
-		{
-			"windwp/nvim-autopairs",
-			config = function()
-				require("nvim-autopairs").setup({
-					disable_filetype = { "TelescopePrompt", "vim" },
-				})
-			end,
-			event = { "InsertEnter" },
-		},
-		{
 			"rmagatti/auto-session",
 			keys = {
 				{
@@ -179,47 +126,9 @@ require("lazy").setup({
 			end,
 		},
 		{
-			"ray-x/lsp_signature.nvim",
-			version = "*",
-		},
-		{
-			"ggandor/leap.nvim",
-			config = function()
-				require("leap").create_default_mappings()
-			end,
-		},
-		{
 			"lervag/vimtex",
 			version = "*",
 			ft = { "tex" },
-		},
-		{
-			"abecodes/tabout.nvim",
-			config = function()
-				require("tabout").setup({})
-			end,
-			lazy = true,
-			-- after = { "nvim-treesitter" },
-		},
-		{
-			"kylechui/nvim-surround",
-			version = "*",
-			event = { "BufRead" },
-			config = function()
-				require("nvim-surround").setup({})
-			end,
-		},
-		{
-			"famiu/bufdelete.nvim",
-			cmd = { "Bdelete", "Bwipeout" },
-		},
-		{
-			"echasnovski/mini.ai",
-			branch = "stable",
-			event = "BufRead",
-			config = function()
-				require("mini.ai").setup()
-			end,
 		},
 		{
 			"tpope/vim-dadbod",
@@ -529,41 +438,6 @@ require("lazy").setup({
 
 				vim.keymap.set({ "n", "v" }, "<leader>ac", ":CodeCompanionChat<cr>", { desc = "AI: Start new chat" })
 				vim.keymap.set({ "n", "v" }, "<leader>at", ":CodeCompanionToggle<cr>", { desc = "AI: Toggle chat" })
-			end,
-		},
-		{
-			"ThePrimeagen/harpoon",
-			branch = "harpoon2",
-			dependencies = { "nvim-lua/plenary.nvim" },
-			config = function()
-				local harpoon = require("harpoon")
-
-				harpoon:setup()
-				vim.keymap.set("n", "<leader>ke", function()
-					harpoon.ui:toggle_quick_menu(harpoon:list())
-				end)
-				vim.keymap.set("n", "<leader>ka", function()
-					harpoon:list():add()
-				end)
-				vim.keymap.set("n", "<M-j>", function()
-					harpoon:list():select(1)
-				end)
-				vim.keymap.set("n", "<M-k>", function()
-					harpoon:list():select(2)
-				end)
-				vim.keymap.set("n", "<M-l>", function()
-					harpoon:list():select(3)
-				end)
-				vim.keymap.set("n", "<M-;>", function()
-					harpoon:list():select(4)
-				end)
-
-				vim.keymap.set("n", "<M-m>", function()
-					harpoon:list():prev()
-				end)
-				vim.keymap.set("n", "<M-,>", function()
-					harpoon:list():next()
-				end)
 			end,
 		},
 	},
