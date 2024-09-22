f5d28b9966389fabdf7fa1a6d8e1e606b78a3e2c
Ennio Mara <enniomara@users.noreply.github.com>
2024-09-22 20:34:00 +0200

    Run formatter on flake.nix

--- flake.nix
+++ flake.nix
@@ -15,95 +15,107 @@
     devshell.url = "github:numtide/devshell";
   };
 
-  outputs = inputs @ { self, nixpkgs, flake-utils, home-manager, darwin, devshell }:
-    let
-      # the devshells used by this repo
-      devshells = (import ./modules/devShell.nix) inputs;
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-utils,
+    home-manager,
+    darwin,
+    devshell,
+  }: let
+    # the devshells used by this repo
+    devshells = (import ./modules/devShell.nix) inputs;
 
-      customOverlays = import ./home-manager/overlays.nix;
-      overlays = [
-        customOverlays.oh-my-zsh
-      ];
+    customOverlays = import ./home-manager/overlays.nix;
+    overlays = [
+      customOverlays.oh-my-zsh
+    ];
 
-      mkDarwinSystem = {
-        userConfigurations,
-        system,
-      }:
-        darwin.lib.darwinSystem {
-          inherit system;
+    mkDarwinSystem = {
+      userConfigurations,
+      system,
+    }:
+      darwin.lib.darwinSystem {
+        inherit system;
 
-          modules = let
-            userConfig =
-              builtins.map (userConfig: {
-                # a new version of home manager broke compatibility with
-                # nix-darwin. It started saying that $HOME as empty.
-                # https://github.com/nix-community/./home-manager/issues/4026
-                # https://github.com/nix-community/./home-manager/issues/4026
-                users.users.${userConfig.username}.home = "/Users/${userConfig.username}";
+        modules = let
+          userConfig =
+            builtins.map (userConfig: {
+              # a new version of home manager broke compatibility with
+              # nix-darwin. It started saying that $HOME as empty.
+              # https://github.com/nix-community/./home-manager/issues/4026
+              # https://github.com/nix-community/./home-manager/issues/4026
+              users.users.${userConfig.username}.home = "/Users/${userConfig.username}";
 
-                home-manager.users.${userConfig.username} = {config, ...}: {
-                  imports =
-                    [
-                      ./home-manager/home.nix
-                      ./home-manager/hammerspoon.nix
-                      ./home-manager/karabiner.nix
-                      ./home-manager/mac-containers.nix
-                    ]
-                    ++ userConfig.imports;
+              home-manager.users.${userConfig.username} = {config, ...}: {
+                imports =
+                  [
+                    ./home-manager/home.nix
+                    ./home-manager/hammerspoon.nix
+                    ./home-manager/karabiner.nix
+                    ./home-manager/mac-containers.nix
+                  ]
+                  ++ userConfig.imports;
 
-                  config = {
-                    # from https://discourse.nixos.org/t/nvd-simple-nix-nixos-version-diff-tool/12397/31
-                    home.activation.report-changes = config.lib.dag.entryAnywhere ''
-                      echo "++++* System Changes ++++++"
-                      nix store diff-closures $(ls -tdv /nix/var/nix/profiles/system-*-link | head -2)
-                    '';
+                config = {
+                  # from https://discourse.nixos.org/t/nvd-simple-nix-nixos-version-diff-tool/12397/31
+                  home.activation.report-changes = config.lib.dag.entryAnywhere ''
+                    echo "++++* System Changes ++++++"
+                    nix store diff-closures $(ls -tdv /nix/var/nix/profiles/system-*-link | head -2)
+                  '';
 
-                    # use the 1password agent to sign commits on mac
-                    programs.git.extraConfig."gpg \"ssh\"".program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
-                  };
+                  # use the 1password agent to sign commits on mac
+                  programs.git.extraConfig."gpg \"ssh\"".program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
                 };
-              })
-              userConfigurations;
-          in
-            [
-              ./home-manager/configuration.nix
-              home-manager.darwinModules.home-manager
-              {
-                nixpkgs.overlays = overlays;
+              };
+            })
+            userConfigurations;
+        in
+          [
+            ./home-manager/configuration.nix
+            home-manager.darwinModules.home-manager
+            {
+              nixpkgs.overlays = overlays;
 
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
 
-                # Create registry so that it can be used in `nix run` commands without downloading upstream nixpkgs again
-                nix.registry.home.flake = nixpkgs;
-              }
-            ]
-            ++ userConfig;
-        };
+              # Create registry so that it can be used in `nix run` commands without downloading upstream nixpkgs again
+              nix.registry.home.flake = nixpkgs;
+            }
+          ]
+          ++ userConfig;
+      };
 
-      mkLinuxSystem = { username, extraModules ? [ ] }: home-manager.lib.homeManagerConfiguration {
+    mkLinuxSystem = {
+      username,
+      extraModules ? [],
+    }:
+      home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
         # Specify your home configuration modules here, for example,
         # the path to your home.nix.
-        modules = [
-          ./home-manager/home.nix
-          ({
-            nixpkgs.overlays = overlays;
-            home.username = username;
-            home.homeDirectory = "/home/${username}";
-            targets.genericLinux.enable = true;
-            home.activation.report-changes = home-manager.lib.hm.dag.entryAnywhere ''
-              echo "++++* System Changes ++++++"
-              nix store diff-closures $(ls -dv /nix/var/nix/profiles/per-user/${username}/home-manager-*-link | tail -2)
-            '';
+        modules =
+          [
+            ./home-manager/home.nix
+            {
+              nixpkgs.overlays = overlays;
+              home.username = username;
+              home.homeDirectory = "/home/${username}";
+              targets.genericLinux.enable = true;
+              home.activation.report-changes = home-manager.lib.hm.dag.entryAnywhere ''
+                echo "++++* System Changes ++++++"
+                nix store diff-closures $(ls -dv /nix/var/nix/profiles/per-user/${username}/home-manager-*-link | tail -2)
+              '';
 
-            # Create registry so that it can be used in `nix run` commands without downloading upstream nixpkgs again
-            nix.registry.home.flake = nixpkgs;
-          })
-        ] ++ extraModules;
+              # Create registry so that it can be used in `nix run` commands without downloading upstream nixpkgs again
+              nix.registry.home.flake = nixpkgs;
+            }
+          ]
+          ++ extraModules;
       };
-    in
+  in
     {
       darwinConfigurations."M-C02G32FSML7H" = mkDarwinSystem {
         system = "aarch64-darwin";
@@ -112,7 +124,7 @@
             username = "marae";
             imports = [
               (import ./home-manager/axis.nix)
-              ({
+              {
                 services.aws-sso = {
                   enable = true;
                   secureStore = "keychain";
@@ -124,7 +136,7 @@
                       - "%s"
                   '';
                 };
-              })
+              }
             ];
           }
         ];
@@ -137,7 +149,7 @@
             username = "marae";
             imports = [
               (import ./home-manager/axis.nix)
-              ({
+              {
                 services.aws-sso = {
                   enable = true;
                   secureStore = "keychain";
@@ -149,12 +161,12 @@
                       - "%s"
                   '';
                 };
-              })
+              }
             ];
           }
           {
             username = "enniomara";
-            imports = [ ./home-manager/personal.nix ];
+            imports = [./home-manager/personal.nix];
           }
         ];
       };
@@ -164,7 +176,7 @@
         userConfigurations = [
           {
             username = "enniomara";
-            imports = [ ./home-manager/personal.nix ];
+            imports = [./home-manager/personal.nix];
           }
         ];
       };
@@ -174,7 +186,7 @@
         username = "marae";
         extraModules = [
           (import ./home-manager/axis.nix)
-          ({
+          {
             # I want to automatically open in my laptop's browser, instead of
             # having to override the BROWSER variable
             services.open-url-via-ssh.automaticBrowserOverride = true;
@@ -188,11 +200,12 @@
                 UrlAction: open
               '';
             };
-          })
+          }
           ./home-manager/agent-forwarding-tmux.nix
         ];
       };
 
-      homeConfigurations."vagrant@linux-box" = mkLinuxSystem { username = "vagrant"; };
-    } // devshells;
+      homeConfigurations."vagrant@linux-box" = mkLinuxSystem {username = "vagrant";};
+    }
+    // devshells;
 }
