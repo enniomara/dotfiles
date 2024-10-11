79ebc62e136bc1a4b3706d8144729cb93e846ce4
Ennio Mara <enniomara@users.noreply.github.com>
2024-10-11 10:14:05 +0200

    Add mapping for opening harpoon marks in telecope

--- nvim/.config/nvim/lua/plugins/editing.lua
+++ nvim/.config/nvim/lua/plugins/editing.lua
@@ -95,9 +95,31 @@ return {
 	{
 		"ThePrimeagen/harpoon",
 		branch = "harpoon2",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
 		config = function()
 			local harpoon = require("harpoon")
+
+			-- basic telescope configuration
+			--  from https://github.com/ThePrimeagen/harpoon/tree/harpoon2?tab=readme-ov-file#telescope
+			local conf = require("telescope.config").values
+			local function toggle_telescope(harpoon_files)
+				local file_paths = {}
+				for _, item in ipairs(harpoon_files.items) do
+					table.insert(file_paths, item.value)
+				end
+
+				require("telescope.pickers").new({}, {
+					prompt_title = "Harpoon",
+					finder = require("telescope.finders").new_table({
+						results = file_paths,
+					}),
+					previewer = conf.file_previewer({}),
+					sorter = conf.generic_sorter({}),
+				}):find()
+			end
+
+			vim.keymap.set("n", "<leader>fh", function() toggle_telescope(harpoon:list()) end,
+				{ desc = "Open harpoon window" })
 
 			harpoon:setup()
 			vim.keymap.set("n", "<leader>ke", function()
