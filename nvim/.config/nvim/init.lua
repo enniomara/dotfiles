d97c4b0c25480c664eb492fc07c6cf762324fda9
Ennio Mara <enniomara@users.noreply.github.com>
2025-02-20 15:19:49 +0100

    Add mapping for accepting copilot suggestion

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -357,7 +357,13 @@ require("lazy").setup({
 				"nvim-treesitter/nvim-treesitter",
 			},
 			version = '*',
-			cmd = "Copilot",
+			config = function()
+				vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+					expr = true,
+					replace_keycodes = false
+				})
+				vim.g.copilot_no_tab_map = true
+			end,
 		}
 	},
 })
