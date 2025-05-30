a4db0029b4cfd0e149e853e155d4eafbae1eaed8
Ennio Mara <enniomara@users.noreply.github.com>
2025-05-30 17:54:59 +0200

    nvim: migrate lsp mappings to vim.keymap.set

--- nvim/.config/nvim/lua/config/lsp/config.lua
+++ nvim/.config/nvim/lua/config/lsp/config.lua
@@ -2,23 +2,33 @@ local M = {}
 
 M.on_attach = function(client, bufnr)
 	local opts = { noremap = true, silent = true }
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(
-		bufnr,
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "Go to Definition" })
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "Go to Declaration" })
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = bufnr, desc = "Go to Implementation" })
+	vim.keymap.set(
 		"n",
-		"<leader>wl",
-		"<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
-		opts
+		"<leader>wa",
+		vim.lsp.buf.add_workspace_folder,
+		{ buffer = bufnr, desc = "Add Workspace Folder" }
 	)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "v", "<leader>ca", "<cmd>lua vim.lsp.buf.range_code_action()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>e", '<cmd>lua vim.diagnostic.open_float({scope="line"})<CR>', opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>fm", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", opts)
+	vim.keymap.set(
+		"n",
+		"<leader>wr",
+		vim.lsp.buf.remove_workspace_folder,
+		{ buffer = bufnr, desc = "Remove Workspace Folder" }
+	)
+	vim.keymap.set("n", "<leader>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, { buffer = bufnr, desc = "List Workspace Folders" })
+	vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code Action" })
+	-- vim.keymap.set("v", "<leader>ca", vim.lsp.buf.range_code_action, { buffer = bufnr, desc = "Range Code Action" })
+	vim.keymap.set("n", "<leader>e", function()
+		vim.diagnostic.open_float({ scope = "line" })
+	end, { buffer = bufnr, desc = "Open Diagnostic Float" })
+	vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { buffer = bufnr, desc = "Set Location List" })
+	vim.keymap.set("n", "<leader>fm", function()
+		vim.lsp.buf.format({ async = true })
+	end, { buffer = bufnr, desc = "Format Code" })
 	vim.cmd([[ command! Format execute 'lua vim.lsp.buf.format({ async = true })' ]])
 
 	if client.server_capabilities.inlayHintProvider then
