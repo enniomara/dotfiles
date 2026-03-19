09edf1d7abbde2259724d7de11859ed6000f1cef
Ennio Mara <enniomara@users.noreply.github.com>
2026-03-19 19:36:18 +0100

    nix: configure docker to store credentials in macOS keychain

--- home-manager/mac-containers.nix
+++ home-manager/mac-containers.nix
@@ -2,5 +2,17 @@
   home.packages = [
     pkgs.colima
     pkgs.docker-client
+    pkgs.docker-credential-helpers
   ];
+
+  home.file.dockerConfig = {
+    target = ".docker/config.json";
+    force = true;
+    text = ''
+      {
+        "currentContext": "colima",
+        "credsStore": "osxkeychain"
+      }
+    '';
+  };
 }
