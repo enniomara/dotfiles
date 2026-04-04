eede6f5c9a2896cc7be17f0ad2195eda4002a381
Ennio Mara <enniomara@users.noreply.github.com>
2026-04-04 22:03:46 +0200

    migrate mac system config

--- home-manager/configuration.nix
+++ home-manager/configuration.nix
@@ -43,37 +43,4 @@
     reattach = true;
     touchIdAuth = true;
   };
-
-  system = {
-    defaults = {
-      NSGlobalDomain = {
-        InitialKeyRepeat = 15; # * 15ms
-        KeyRepeat = 2; # * 15ms
-        ApplePressAndHoldEnabled = false; # Turn off accent menu on hold
-        AppleInterfaceStyleSwitchesAutomatically = true; # auto dark mode
-      };
-
-      dock = {
-        autohide = false;
-        appswitcher-all-displays = true;
-        tilesize = 33;
-      };
-
-      finder = {
-        AppleShowAllExtensions = true;
-        AppleShowAllFiles = true; # by default finder hides files starting with . (dot)
-        ShowPathbar = true; # show the breadcrunb
-      };
-
-      trackpad = {
-        Clicking = true; # tap to click
-        FirstClickThreshold = 0; # light clicking, default is medium
-      };
-    };
-
-    keyboard = {
-      enableKeyMapping = true;
-      remapCapsLockToEscape = true;
-    };
-  };
 }
