903113b678f88b06c00f4c49b2766a109b6c8f32
Ennio Mara <enniomara@users.noreply.github.com>
2026-04-04 23:18:00 +0200

    migrate darwin primary user

--- modules/den.nix
+++ modules/den.nix
@@ -21,12 +21,17 @@
 
   den.schema.user.classes = lib.mkDefault ["homeManager"];
 
-  den.hosts.aarch64-darwin.M-K6P79MG3J6.users.marae = { };
+  den.hosts.aarch64-darwin.M-K6P79MG3J6.users.marae = {};
   den.hosts.aarch64-darwin.M-K6P79MG3J6.users.enniomara = {};
   den.homes.x86_64-linux."marae@pcczc65196q9" = {};
 
+  den.aspects.M-K6P79MG3J6 = {
+    darwin = {
+      system.primaryUser = "marae";
+    };
+  };
 
-  den.aspects.marae = {
+  den.aspects.marae = den.lib.parametric.exactly {
     includes = [
       den.provides.hostname
       me.darwin-common
