a9e34e1a819f70bb96c7a793c7b69d85cffe4433
Ennio Mara <enniomara@users.noreply.github.com>
2026-04-04 11:49:43 +0200

    migrate zsh

--- home-manager/configuration.nix
+++ home-manager/configuration.nix
@@ -16,9 +16,6 @@
   nixpkgs.flake.setFlakeRegistry = false;
   nixpkgs.flake.setNixPath = false;
 
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.zsh.enable = true; # default shell on catalina
-
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
