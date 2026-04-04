9903dae034eb82e086f1c823ff0aa20c377861ed
Ennio Mara <enniomara@users.noreply.github.com>
2026-04-04 11:35:29 +0200

    migrate jujutsu

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -76,97 +76,6 @@
     };
   };
 
-  programs.jujutsu = {
-    enable = true;
-    package = pkgs.nixpkgs-unstable.jujutsu;
-    settings = {
-      signing = {
-        behaviour = "drop"; # commits are signed on push (via git.sign-on-push)
-        backend = "ssh";
-        key = "~/.ssh/id_ed25519.pub";
-        backends = {
-          ssh = lib.mkIf pkgs.stdenv.isDarwin {
-            program = "/Applications/1Password.app/Contents/MacOS/op-ssh-sign";
-          };
-        };
-      };
-
-      git = {
-        sign-on-push = true;
-      };
-
-      templates = {
-        # show the diff in the editor when drafting description. I.e. similar to git commit -v
-        draft_commit_description = ''
-          concat(
-            builtin_draft_commit_description,
-            "\nJJ: ignore-rest\n",
-            diff.git(),
-          )
-        '';
-      };
-
-      template-aliases = {
-        # from https://github.com/jj-vcs/jj/issues/7702
-        "list_first(list)" = "stringify(list.join('\n')).first_line()";
-        "first_bookmark_name(bookmarks)" = "list_first(bookmarks.map(|b| b.name()))";
-      };
-
-      revset-aliases = {
-        "closest_bookmark(to)" = "heads(::to & bookmarks())";
-      };
-
-      aliases = {
-        tug = ["bookmark" "move" "--from" "heads(::@- & bookmarks())" "--to" "@-"];
-        # PR create
-        prc = [
-          "util"
-          "exec"
-          "--"
-          "bash"
-          "-c"
-          # If we are using a fork, then gh pr create needs the full ref, i.e. <user>:<branch>
-          # bash
-          ''
-            gh pr create --head "$(gh api user -q .login):$(jj log -r 'closest_bookmark(@)' -T 'bookmarks' --no-graph | cut -d ' ' -f 1)"
-          ''
-        ];
-        prw = [
-          "util"
-          "exec"
-          "--"
-          "bash"
-          "-c"
-          # sleep to allow time for github to register the new PR and start
-          # checks. Otherwise it returns no checks found
-          # bash
-          ''
-            sleep 5 && \
-            gh pr checks --watch $(
-              gh pr list \
-                --head $(jj log -r 'closest_bookmark(@)' -T 'first_bookmark_name(remote_bookmarks)' --no-graph) \
-                --json number \
-                -q '.[0].number'
-            ) && \
-            osascript -e 'display notification "PR checks finished" with title "Github PR"'
-          ''
-        ];
-        gf = ["git" "fetch" "--all-remotes"];
-        c = "commit";
-      };
-
-      "--scope" = [
-        {
-          when.commands = ["diff" "show"];
-          ui = {
-            pager = "delta";
-            diff-formatter = ":git";
-          };
-        }
-      ];
-    };
-  };
-
   xdg.configFile."git/ignore" = {
     # By default git looks here as the last step when ignoring files. This file
     # acts as a global gitignore
@@ -175,7 +84,6 @@
       .aider*
       !.aider.conf.yml
       !.aiderignore
-      .jj
     '';
   };
 
@@ -265,7 +173,5 @@
     (pkgs.writeShellScriptBin "review" (builtins.readFile ../bin/review.bash))
 
     pkgs.gh-dash
-    pkgs.lazyjj
-    pkgs.jjui
   ];
 }
