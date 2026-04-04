80bda7fa39d6626113c391033b959085403661e8
Ennio Mara <enniomara@users.noreply.github.com>
2026-04-04 22:21:09 +0200

    add home-common aspect

---
+++ modules/homeCommon.nix
@@ -0,0 +1,54 @@
+{
+  me.home-common = {
+    homeManager = {pkgs, ...}: {
+      programs.home-manager.enable = true;
+      programs.direnv = {
+        enable = true;
+        nix-direnv.enable = true;
+      };
+
+      home.sessionPath = [
+        "$HOME/.local/bin"
+      ];
+
+      home.packages = [
+        pkgs.watch
+        pkgs.coreutils
+        pkgs.findutils
+
+        pkgs.htop
+
+        # cli
+        pkgs.autojump
+        pkgs.fd
+        pkgs.jq
+        pkgs.jqp # live jq
+        pkgs.ripgrep
+        pkgs.tldr
+        pkgs.kubectl
+        pkgs.k9s
+        pkgs.curl
+        pkgs.go-task
+        pkgs.uv
+
+        pkgs.lazydocker
+
+        pkgs.awscli2
+        pkgs.ssm-session-manager-plugin
+
+        pkgs.rustup
+        pkgs.duckdb
+
+        pkgs._1password-cli
+
+        (pkgs.nixpkgs-unstable.python3.withPackages (ps:
+          with ps; [
+            llm
+            llm-github-copilot
+          ]))
+
+        (pkgs.writeShellScriptBin "review" (builtins.readFile ../bin/review.bash))
+      ];
+    };
+  };
+}
