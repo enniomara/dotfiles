468f7431398989587de3754b89ef5bdb507fb385
Ennio Mara <enniomara@users.noreply.github.com>
2026-04-06 18:45:38 +0200

    move hosts config to hosts.nix

--- modules/den.nix
+++ modules/den.nix
@@ -27,58 +27,6 @@
   den.schema.user.classes = lib.mkDefault ["homeManager"];
   den.ctx.user.includes = [den._.mutual-provider];
 
-  den.aspects.M-K6P79MG3J6 = {
-    includes = [
-      me.determinate
-      den.provides.mutual-provider
-    ];
-
-    provides.marae = {
-      includes = [
-        den.provides.primary-user
-        (me.aws {
-          secureStore = "keychain";
-          extraConfig = ''
-            UrlAction: open-url-in-container
-            ConfigProfilesUrlAction: open-url-in-container
-            UrlExecCommand:
-              - /Applications/Firefox.app/Contents/MacOS/firefox
-              - "%s"
-          '';
-        })
-        (me.open-url-via-ssh {
-          # i only want the sshWithBrowser command available
-          enableAutomaticBrowserOverride = false;
-        })
-      ];
-    };
-  };
-
-  den.aspects."marae@pcczc65196q9" = {
-    includes = [
-      me.agentForwardingTmux
-    ];
-
-    provides.marae = {
-      includes = [
-        (me.aws {
-          secureStore = "file";
-          # if url opening doesn't work, make sure that the GUI services in
-          # ubuntu are not started (i.e. when a user is logged in through
-          # the gui)
-          extraConfig = ''
-            UrlAction: open
-          '';
-        })
-        (me.open-url-via-ssh {
-          # I want to automatically open in my laptop's browser, instead of
-          # having to override the BROWSER variable
-          enableAutomaticBrowserOverride = true;
-        })
-      ];
-    };
-  };
-
   den.aspects.marae = {
     includes = [
       den.provides.hostname
